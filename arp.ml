09844dd6cbd70698efbb0d5874a29831e6047428
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2014-10-21 11:53:22 +0100

    refined the behaviour a bit, such as the definition of the cache; evolved the interface, and marked the bits where we need to interact with a network interface; added lots of comments;

--- arp.ml
+++ arp.ml
@@ -1,17 +1,132 @@
+(*
+   We want to represent a partial function:
+   IP_addr -> MAC_addr
+   But the evaluation of this function is weird: we don't have all of its
+   evaluatio at the same time. Moreover, its value can vary over time, as hosts
+   join and leave the network.
+
+   Ideas:
+   - If we cannot complete the evaluation, then just return None and have the
+   client try again in the future. In the mean time, we will try and obtain a
+   value from the network. This is the behaviour suggested by RFC826, under
+   "Packet Generation".
+   - If we cannot complete the evaluation, then return immediately to the client
+   with a subscription to a waiting list that will be advised as soon as we have
+   a value (or will timeout).
+   - Block until we can complete the evaluation (or time out).
+
+   In each case, what's persistent, and what's transient?
+   - Persistent: current config of the database: mappings from IPs to MACs.
+       Also include waiting lists?
+       Also, what needs to be done in case db is reloaded?
+        This reduces to figuring out what to do in case a MAC address for an IP
+          address is updated. Do we go back in time and tell every past caller?
+          Or do we rely on them to recheck?
+          This is not really needed, since we expire values based on age.
+          So in the worst case, we could start with an empty database.
+   - Transient: timeouts of lookups, ages of values, and associated state.
+
+  How is all this different from the state info that's kept at other layers in
+   the network stack? To be determined...
+*)
+
 (*NOTE I'm assuming a 64-bit precision for "int".*)
 type ip_addr = int;; (*32 bits needed*)
 type mac_addr = int;; (*48 bits needed*)
 
-type state = (ip_addr, mac_addr) Hashtbl.t;;
+(*NOTE this implementation is specialised for Ethernet and IP,
+  i.e., using the language of RFC826 I'm assuming that
+  ar$pro={ether_type$DOD_INTERNET} and ar$hrd={ares_hrd$Ethernet}.
+  Consequently, it should always be the case that ar$hln=6 (bytes) and ar$pln=4.
+*)
+
+(*NOTE I'm assuming that ethernet packet has been parsed according to
+  "Packet format" section of RFC826. For ARP code to be run, the Protocol Type
+  field of the Ethernet packet header must be ether_type$ADDRESS_RESOLUTION.
+*)
+
+(*
+  NOTE (needs to be refined)
+   Assumes source-related info is correct.
+   Ignores target-related info, even if arest_op$REPLY.
+*)
+
+(* From RFC826:
+"The protocol described in this paper distributes information as
+it is needed, and only once (probably) per boot of a machine."
+
+This feature is really cool:
+"This format allows the packet buffer to be reused if a reply is
+generated; a reply has the same length as a request, and several
+of the fields are the same."
+
+Notes for parsers:
+  "In theory, the length fields (ar$hln and ar$pln) are redundant,
+  since the length of a protocol address should be determined by
+  the hardware type (found in ar$hrd) and the protocol type (found
+  in ar$pro).  It is included for optional consistency checking,
+  and for network monitoring and debugging (see below)."
+
+  "The opcode is to determine if this is a request (which may cause
+  a reply) or a reply to a previous request.  16 bits for this is
+  overkill, but a flag (field) is needed."
+
+  "The sender hardware address and sender protocol address are
+  absolutely necessary.  It is these fields that get put in a
+  translation table."
+*)
+
+(*
+TODO
+- Timeout - for query REQUEST
+- Aging - reverse it when receive packets from that host
+- Deletion of entry upon detection of unreachability
+- Implementing the 5527 protocol: probing to check if the address is available,
+   etc.
+- Allowed number of retransmitions, and min&max gaps between them.
+*)
+
+(*Raw ARP opcodes*)
+(*FIXME classify them into more refined set of operations, based on RFC5527?*)
+type arp_op =
+  | Request (*ares_op$REQUEST*)
+  | Reply (*ares_op$REPLY*)
+
+(*NOTE excludes Ethernet header info*)
+type arp_packet_format = {
+  ar_hrd : int; (*NOTE must be = 1*)
+  ar_pro : int; (*NOTE must be = 6*)
+  ar_hln : int; (*NOTE must be = 6*)
+  ar_pln : int; (*NOTE must be = 4*)
+  ar_op : arp_op;
+  ar_sha : int;
+  ar_spa : int;
+  ar_tha : int;
+  ar_tpa : int;
+}
+
+type timestamp = float (*FIXME or use Int64.t?*)
+
+type entry_state =
+  (*Address value, and the (local) timestamp the value was added.
+    The timestamp can be used to purge the value, and/or request a refreshing,
+    if the value is deemed to be too old.*)
+  | Result of mac_addr * timestamp
+  (*Indication that we don't have the info yet, but we also provide the time
+    when we requested the info, to be used for timing out.*)
+  | Waiting of timestamp
+
+type state = (ip_addr, entry_state) Hashtbl.t;;
 
 (*Initial size of the hashtable*)
 let kINIT_HT_SIZE = 0;;
 
 let empty_state () : state = Hashtbl.create ~random:false kINIT_HT_SIZE;;
 
+(*FIXME make private*)
 (*Adds an address pair to the cache.*)
 let cache state (ip_addr, mac_addr) =
-  let state' = Hashtbl.copy state in
+  let state' = Hashtbl.copy state in (*NOTE duplication*)
   Hashtbl.add state' ip_addr mac_addr;
   state';;
 
@@ -24,6 +139,54 @@ let lookup state ip_addr =
        network*)
     None;;
 
+(*FIXME need network interface*)
+let send (p : arp_packet_format) (*interface*) =
+  failwith "TODO"
+;;
+let my_ip_address : int = 0;; (*FIXME*)
+let my_mac_address : int = 0;; (*FIXME*)
+
+(*Implements the algorithm described under "Packet Reception" in RFC826*)
+let receive state (p : arp_packet_format) =
+  (*NOTE these invariants were stated in comments earlier*)
+  assert (p.ar_hrd = 1);
+  assert (p.ar_pro = 6);
+  assert (p.ar_hln = 6);
+  assert (p.ar_pln = 4);
+  (*FIXME use polymorphic variants instead of ints. Can then implement the
+    checks in the algorithm more easily -- e.g., "?Do I have the hardware type
+    in ar$hrd?"*)
+  let merge_flag =
+    if Hashtbl.mem state p.ar_spa then
+      begin
+        Result (p.ar_sha, Unix.time ())
+        |> Hashtbl.replace state p.ar_spa;
+        true
+      end
+    else false in
+  if p.ar_tpa = my_ip_address then
+    begin
+      if not merge_flag then
+        Result (p.ar_sha, Unix.time ())
+        |> Hashtbl.add state p.ar_spa;
+
+      if p.ar_op = Request then
+        {
+          ar_hrd = 1;
+          ar_pro = 6;
+          ar_hln = 6;
+          ar_pln = 4;
+          ar_op = Reply;
+          ar_sha = my_mac_address;
+          ar_spa = my_ip_address;
+          ar_tha = p.ar_sha;
+          ar_tpa = p.ar_spa;
+        }
+        |> send
+    end
+;;
+
+(*FIXME purge?*)
 (*Walks the cache, removing expired entries.*)
 let expire state = failwith "TODO";;
 
--- arp.mli
+++ arp.mli
@@ -1,20 +1,36 @@
-(* ARP cache *)
+(* ARP cache.
+   Based on RFC826
+   Specialised to IP.
+*)
 
-type ip_addr = int
-type mac_addr = int
+
+(*FIXME representation of these values too transparent?*)
+type ip_addr = int (*32 bits needed to represent IP address -- assuming v4*)
+type mac_addr = int (*48 bits are needed to represent a MAC address*)
 
 type state
 
 val empty_state : () -> state
 
-(*FIXME use standard names for these functions, from standard APIs*)
+(*FIXME use standard names for these functions, from standard APIs
+  @samoht suggested using this as reference:
+  https://github.com/mirage/mirage-tcpip/blob/master/lib/arpv4.mli
+*)
 
 (*Adds an address pair to the cache.*)
 val cache : state -> ip_addr * mac_addr -> state
 
 (*Might need to query the network to perform the lookup.*)
 val lookup : state -> ip_addr -> mac_addr option
+(*it's OK to return None. From RFC826:
+    "If it does not [find the IP address in the resolution table], it probably informs the caller that it is throwing the
+packet away (on the assumption the packet will be retransmitted
+by a higher network layer), and generates an Ethernet packet with
+a type field of ether_type$ADDRESS_RESOLUTION."
+*)
 
 (*Walks the cache, removing expired entries*)
+(*FIXME might be more economical to expire entries upon lookup -- but then risk
+getting a leak if the network is large and if hosts pop on and off.*)
 val expire : state -> state
 
